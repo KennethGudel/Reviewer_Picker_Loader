eae3c1466c574ba2afecbd68f4866c9ec049e310
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2016-01-06 15:20:30 -0500

    adds atomic alias change, closes #10

--- Loader.py
+++ Loader.py
@@ -15,7 +15,16 @@ def put(doc, index_name, number, es):
 def alias(index_name, alias_name, es):
 	if es.indices.exists_alias(name=alias_name) == True:
 		al_cur = es.indices.get_alias(name=alias_name)
-		es.indices.delete_alias(index='_all', name=alias_name)
-		es.indices.put_alias(index=index_name, name=alias_name)
+		body = atomic_alias(alias_name, al_cur, index_name)
+		es.indices.update_aliases(body=body)
 	else:
 		es.indices.put_alias(index=index_name, name=alias_name)
+
+def atomic_alias(alias_name, al_cur, index_name):
+	body = '''{
+		"actions" : [
+			{"remove" : { "index" : "%s", "alias": "%s"}},
+			{"add" : { "index" : "%s", "alias": "%s"}}
+		]
+	}''' % (al_cur.keys()[0], alias_name, index_name, alias_name)
+	return body
